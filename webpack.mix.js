7e5c1180ffa61959b85ed6da043593ef583c8451
vytrang <clara@pstworld.com>
2019-05-23 20:38:48 +0700

    Create layout for frontend

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,13 +12,13 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css');
-
-mix.js('resources/js/bootstrap.min.js', 'public/layouts/js')
+    .sass('resources/sass/app.scss', 'public/css')
+    .js('resources/js/bootstrap.min.js', 'public/layouts/js')
     .js('resources/js/jquery.js', 'public/layouts/js')
     .js('resources/js/jquery.scrollUp.min.js', 'public/layouts/js')
     .js('resources/js/main.js', 'public/layouts/js')
     .styles('resources/css/bootstrap.min.css', 'public/layouts/css/bootstrap.min.css')
     .styles('resources/css/font-awesome.min.css', 'public/layouts/css/font-awesome.min.css')
     .styles('resources/css/main.css', 'public/layouts/css/main.css')
-    .styles('resources/css/responsive.css', 'public/layouts/css/responsive.css'); 
+    .styles('resources/css/responsive.css', 'public/layouts/css/responsive.css')
+    .copy('resources/fonts/', './public/layouts/fonts/', false);
