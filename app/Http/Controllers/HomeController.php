a46263828f6faae3570869a961f4fcd3976d06b1
vytrang <clara@pstworld.com>
2019-06-07 20:29:19 +0700

    Create home page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Product;
+use App\Brand;
 
 class HomeController extends Controller
 {
@@ -13,7 +15,7 @@ class HomeController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth');
+        // $this->middleware('auth');
     }
 
     /**
@@ -23,6 +25,9 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $products = Product::orderBy('created_at', 'DESC')->get();
+        $brands = Brand::inRandomOrder()->take(config('products.brand_on_home'))->get();
+
+        return view('index', ['products' => $products, 'brands' => $brands]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -11,13 +11,11 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'HomeController@index')->name('home');
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+// Route::get('/home', 'HomeController@index')->name('home');
 
 Route::group([
     'namespace' => 'Admin',
